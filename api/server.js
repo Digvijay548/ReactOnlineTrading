3f438dd2a00e057c70540d9e41126fc1cbe43097
Digvijay Patil <160723513+Digvijay548@users.noreply.github.com>
2025-02-22 11:36:16 +0000

    Adjust payment balance get online and add balance

--- api/server.js
+++ api/server.js
@@ -17,14 +17,14 @@ const corsOptions = {
   allowedHeaders: "Content-Type, Authorization",
   credentials: true
 };
-//app.use(cors(corsOptions));
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins (or specify domain)
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+//app.use((req, res, next) => {
+ // res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins (or specify domain)
+ // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+ // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+ // next();
+//});
 
 app.use(express.json()); // For parsing application/json
 
