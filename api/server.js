23b4ea4f7c9e42ff3b8b066cc967308fa08b7795
Digvijay Patil <160723513+Digvijay548@users.noreply.github.com>
2025-02-24 19:11:28 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
   allowedHeaders: "Content-Type, Authorization",
   credentials: true
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 //app.use((req, res, next) => {
  // res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins (or specify domain)
